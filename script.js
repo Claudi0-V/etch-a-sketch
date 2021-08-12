26ee3a47179698e340c7e7de74aedf56825fa1c8
Claudioub16 <claudioub16@gmail.com>
2021-08-12 15:55:55 -0300

    added the clear function, replace in the css the amount of grid-templates for a var that is changed from the js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const gridContainer = document.querySelector('#grid-container');
+const clearButton = document.querySelector('#clear-grid');
+const gridDeffiner = () => document.querySelectorAll('.cell');
 
 
-function createGrid(row, column) {
-
-const gridArr = [...Array(row * column).keys()];
-gridArr.forEach(value => {
-	div = document.createElement(`div`);
-	div.classList.add('cell');
-	gridContainer.appendChild(div);
-})
-
+function createGrid(rows) {
+	const gridArr = [...Array(rows * rows).keys()];
+	gridArr.forEach(value => {
+		div = document.createElement(`div`);
+		div.classList.add('cell');
+		gridContainer.appendChild(div);
+	})
 }
 
 
@@ -17,11 +17,28 @@ function hoveringStyle(e) {
 	e.target.classList.add('hovered');
 }
 
-createGrid(16, 16);
 
+function newGridRows() {
+	let newGrid = 0;
+	do {
+		newGrid = parseInt(prompt('What is the size of the new grid (Max 100)?'));
+	} while (newGrid > 100);
+	return newGrid;
+}
 
-const gridCells = document.querySelectorAll('.cell');
+function resetStyle() {
+	gridCells.forEach(cell => cell.classList.remove('hovered'));
+	let cells = document.querySelectorAll('.cell');;
+	cells.forEach(cell => gridContainer.removeChild(cell))
+	let newGrid = newGridRows();
+	createGrid(newGrid);	
+	const newGridCells = gridDeffiner();
+	newGridCells.forEach(cell => cell.addEventListener('mouseover', hoveringStyle))
+	document.documentElement.style.setProperty('--row', `${newGrid}`);
+}
 
+createGrid(16);
+const gridCells = gridDeffiner();
 
-
-gridCells.forEach(cell => cell.addEventListener('mouseover', hoveringStyle))
+gridCells.forEach(cell => cell.addEventListener('mouseover', hoveringStyle));
+clearButton.addEventListener('click', resetStyle);
