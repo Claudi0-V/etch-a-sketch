285f39897f8d3c309d8f0ba4af7b05fc7803bfc3
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-11-25 17:17:18 -0300

    changed the logic on the createGrid, removed unnecesary functions

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
-const clearButton = document.querySelector('#clear-grid');
-const main = document.querySelector('main');
-const buttonsDiv = document.querySelector('#buttons-div');
+const clearButton = document.querySelector("#clear-grid");
+const main = document.querySelector("main");
+const buttonsDiv = document.querySelector("#buttons-div");
+const colorize = false;
 
-const getGridCells = () => document.querySelectorAll('.cell');
+const getGridCells = () => document.querySelectorAll(".cell");
 
 const createGrid = (rows) => {
-	const gridContainer = document.querySelector('#grid-container');
-	gridContainer.remove();
-	const newGrid = document.createElement('div');
-	newGrid.setAttribute('id', 'grid-container');
-	for (let i = 0; i < rows * rows; i++) {
-		const div = document.createElement('div');
-		div.classList.add('cell');
-		newGrid.appendChild(div);
-	}
-	buttonsDiv.parentNode.insertBefore(newGrid, buttonsDiv.nextSibling)
-}
+  if (rows < 2 && grid > 100) rows = 16;
+  const gridContainer = document.querySelector("#grid-container");
+  gridContainer.remove();
+  const newGrid = document.createElement("div");
+  newGrid.setAttribute("id", "grid-container");
+  for (let i = 0; i < rows * rows; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("mousemove", changeCellBackground);
+    div.classList.add("cell");
+    newGrid.appendChild(div);
+  }
+  buttonsDiv.parentNode.insertBefore(newGrid, buttonsDiv.nextSibling);
+};
 
-const hoveringStyle = (e) => {
-	e.target.classList.add('hovered');
-}
+const changeCellBackground = (event) => {
+  event.target.style.backgroundColor = "black";
+};
 
 const newGridRows = () => {
-	let newGrid;
-	do {
-		newGrid = parseInt(prompt('What is the size of the new grid (Min 1, Max 100)?'));
-	} while (newGrid > 100 || newGrid < 1);
-	return newGrid;
-}
-
-const newGrid = () => {
-	const newGrid = newGridRows();
-	createGrid(newGrid);	
-	const newGridCells = getGridCells();
-	newGridCells.forEach(cell => cell.addEventListener('mouseover', hoveringStyle))
-	document.documentElement.style.setProperty('--rows', `${newGrid}`);
-}
+  let newGrid;
+  do {
+    newGrid = parseInt(
+      prompt("What is the size of the new grid (Min 1, Max 100)?")
+    );
+  } while (newGrid > 100 || newGrid < 1);
+  return newGrid;
+};
 
 createGrid(16);
 const gridCells = getGridCells();
 
-gridCells.forEach(cell => cell.addEventListener('mouseover', hoveringStyle));
-clearButton.addEventListener('click', newGrid);
+clearButton.addEventListener("click", newGrid);
+
