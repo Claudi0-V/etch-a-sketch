fb6e6edcca004b891b9ed34d743b9f2605f61881
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-11-26 10:21:05 -0300

    added favicon and changed the structure of the grid

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
 const clearButton = document.querySelector("#clear-grid");
 const main = document.querySelector("main");
 const buttonsDiv = document.querySelector("#buttons-div");
-const colorize = false;
+const gridContainer = document.querySelector("#grid-container");
 
-const getGridCells = () => document.querySelectorAll(".cell");
+const colorize = false;
 
 const createGrid = (rows) => {
   if (rows < 2 && grid > 100) rows = 16;
-  const gridContainer = document.querySelector("#grid-container");
-  gridContainer.remove();
+  const grid = document.querySelector(".grid");
+  grid.remove();
   const newGrid = document.createElement("div");
-  newGrid.setAttribute("id", "grid-container");
+  newGrid.setAttribute("class", "grid");
   for (let i = 0; i < rows * rows; i++) {
     const div = document.createElement("div");
     div.addEventListener("mousemove", changeCellBackground);
     div.classList.add("cell");
-    newGrid.appendChild(div);
+    grid.appendChild(div);
   }
-  buttonsDiv.parentNode.insertBefore(newGrid, buttonsDiv.nextSibling);
+  gridContainer.append(grid);
 };
 
 const changeCellBackground = (event) => {
   event.target.style.backgroundColor = "black";
 };
 
-const newGridRows = () => {
-  let newGrid;
-  do {
-    newGrid = parseInt(
-      prompt("What is the size of the new grid (Min 1, Max 100)?")
-    );
-  } while (newGrid > 100 || newGrid < 1);
-  return newGrid;
-};
-
 createGrid(16);
-const gridCells = getGridCells();
-
-clearButton.addEventListener("click", newGrid);
-
+clearButton.addEventListener("click", createGrid);
